6e9d9c9b245e528ef0edd95e5c568dfdfb4aede5
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-04-23 11:55:42 +0700

    Some change

--- CSharp_WinForms_POS_And_Quotation_System/ProductManagementForm.Designer.cs
+++ CSharp_WinForms_POS_And_Quotation_System/ProductManagementForm.Designer.cs
@@ -28,21 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel3 = new Panel();
+            label1 = new Label();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            panel3.SuspendLayout();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.InactiveCaption;
+            panel3.Controls.Add(label1);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(0, 0);
+            panel3.Margin = new Padding(2, 3, 2, 3);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1084, 60);
+            panel3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Tahoma", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(8, 8);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(179, 39);
+            label1.TabIndex = 0;
+            label1.Text = "จัดการสินค้า";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 1138);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1084, 23);
+            statusStrip1.TabIndex = 4;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Font = new Font("Tahoma", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(52, 18);
+            toolStripStatusLabel1.Text = "รายการ";
             // 
             // ProductManagementForm
             // 
             AutoScaleDimensions = new SizeF(8F, 18F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1084, 1161);
+            Controls.Add(statusStrip1);
+            Controls.Add(panel3);
             Font = new Font("Tahoma", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
             Margin = new Padding(3, 4, 3, 4);
             Name = "ProductManagementForm";
             Text = "Product Management";
             Load += ProductManagementForm_Load;
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel3;
+        private Label label1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
