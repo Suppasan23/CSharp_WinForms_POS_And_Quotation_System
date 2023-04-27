502e79494af1425f2a365a69094c2c6445aa8151
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-04-27 11:43:38 +0700

    27-Apr-23 11:43AM

--- CSharp_WinForms_POS_And_Quotation_System/ArchiveQuotationForm.Designer.cs
+++ CSharp_WinForms_POS_And_Quotation_System/ArchiveQuotationForm.Designer.cs
@@ -28,21 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            panel3 = new Panel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1084, 1161);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(219, 138, 138);
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1082, 60);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(8, 8);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(259, 39);
+            label1.TabIndex = 0;
+            label1.Text = "ที่เก็บใบเสนอราคา";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(251, 240, 240);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(0, 60);
+            panel3.Margin = new Padding(3, 2, 3, 2);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1082, 1099);
+            panel3.TabIndex = 2;
             // 
             // ArchiveQuotationForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1084, 1161);
+            Controls.Add(panel1);
             Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
             Margin = new Padding(3, 4, 3, 4);
             Name = "ArchiveQuotationForm";
             Text = "Archive Quotation";
             Load += ArchiveQuotationForm_Load;
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel3;
+        private Panel panel2;
+        private Label label1;
     }
 }
--- CSharp_WinForms_POS_And_Quotation_System/CreateQuotationForm.Designer.cs
+++ CSharp_WinForms_POS_And_Quotation_System/CreateQuotationForm.Designer.cs
@@ -28,21 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            panel3 = new Panel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1084, 1161);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(192, 232, 74);
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1082, 60);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(8, 8);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(251, 39);
+            label1.TabIndex = 0;
+            label1.Text = "สร้างใบเสนอราคา";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(250, 253, 236);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(0, 60);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1082, 1099);
+            panel3.TabIndex = 2;
             // 
             // CreateQuotationForm
             // 
             AutoScaleDimensions = new SizeF(8F, 18F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1084, 1161);
+            Controls.Add(panel1);
             Font = new Font("Tahoma", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
             Margin = new Padding(3, 4, 3, 4);
             Name = "CreateQuotationForm";
             Text = "Create Quotation";
             Load += CreateQuotationForm_Load;
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel3;
+        private Panel panel2;
+        private Label label1;
     }
 }
